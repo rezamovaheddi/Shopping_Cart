4accc603b3d8c46b4278b9222b493d2e58bcf35a
rezamovahedi <rmwhdy8@gmail.com>
2024-11-01 13:35:10 +0330

    add leyout and making photo

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import ProductList from './components/ProductList'
+// import './App.css'
+
+function App() {
+  return <div>
+    <header className='w-full bg-black'>
+      <ul className='text-white flex justify-between'>
+        <li className='m-2 border-2 rounded-md bg-white text-black w-12 text-center font-medium shadow-inner shadow-black hover:bg-lime-400'>login</li>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 m-2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+        </svg>
+      </ul>
+      <div className='flex justify-center items-center p-24'>
+        <h1 className='text-white font-semibold text-4xl mb-5'>Reza Shop</h1>
+      </div>
+    </header>
+    <ProductList>
+      
+    </ProductList>
+  </div>
+}
+
+export default App
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,33 @@
+const PRODUCT = [
+    {
+        name:'mobile',
+        id:1,
+        price:123,
+        image:''
+    },
+    {
+        name:'laptop',
+        id:2,
+        price:400,
+        image:''
+    },
+    {
+        name:'camera',
+        id:3,
+        price:44,
+        image:''
+    },
+    {
+        name:'shose',
+        id:5,
+        price:700,
+        image:''
+    }
+]
+
+const ProductList = () => {
+  return (
+    <div>ProductList</div>
+  )
+}
+export default ProductList
