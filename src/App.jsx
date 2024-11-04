6b055cd1e73253fc60c6e2d7c1093d0eeb248f3e
rezamovahedi <rmwhdy8@gmail.com>
2024-11-04 21:02:35 +0330

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,16 @@ function App() {
 
 function AddToCart(products) {
  setAddProduct(prevItems => {
-      const existingProduct = prevItems.find(item => item.id === products.id);
+      const existingProduct = prevItems.find((item)=> item.id === products.id);
       if (existingProduct) {
-          return prevItems.map(item =>
+          return prevItems.map(item => (
           item.id === products.id
-            ? { ...item, quantity: item.quantity + 1 }
+            ? { ...item, quantity: item.quantity + 1}
             : item
+          )
         )
       } else [...prevItems, { ...products, quantity: 1 }];
     });
-
     setCartItems(preveCart => {
       const isCartToItem = preveCart.find(item=> item.id === products.id)
       if(isCartToItem){
@@ -29,9 +29,8 @@ function AddToCart(products) {
           ? {...item,quantity:item.quantity + 1}
           : item
         )
-      }else{
-        return [...preveCart, {...products,quantity:1}]
-      }
+      }else return [...preveCart, {...products,quantity:1}]
+    
     })
 }
 // function deleteItem(items){
