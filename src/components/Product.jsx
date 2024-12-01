06a1418efb5c87886811ee9bcc203d58b8f428c1
rezamovahedi <rmwhdy8@gmail.com>
2024-12-01 21:35:59 +0330

    add api to card items

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-const Product = ({el ,onAdd}) => {
+import React from "react";
+const Product = ({ el, onAdd }) => {
   return (
     <div>
-        <div className='hover:shadow-lg transition-all hover:mt-2'>
-            <div className='border-4 rounded-md'>
-                <img className='w-80 p-2 m-4' src={el.image} width={100} alt={el.image}/>
-            <div className='flex justify-between'>
-             <span className='text-2xl font-semibold ml-2'>{el.name}</span>
-             <span className='text-red-600 font-semibold'>{el.price}$</span>
-            </div>
-            <div className='flex justify-center items-center w-full bg-slate-300 p-3 rounded-md transition-shadow transition-colors hover:shadow-xl hover:bg-green-400'>
-              <button className='font-semibold text-xl text-slate-800' onClick={()=> onAdd(el)}>buy</button>
-            </div>
-            </div>
-          
+      <div className="hover:shadow-lg transition-all hover:shadow-2xl flex">
+        <div className="card card-compact shadow-lg col-span-1 w-96 h-fit bg-gray-50 hover:bg-base-200 shadow-gray-400 rounded-2xl">
+          <img  
+            className="w-16 p-1 m-2"
+            src={el.image}
+            width={100}
+            alt={el.image}
+          />
+          <span>{el.title}</span>
+          <div className="flex justify-between">
+            <span className="text-xs font-bold ml-2">{el.name}</span>
+            <span className="text-red-600 font-semibold">{el.price}$</span>
+          </div>
+          <div className="flex justify-center items-center w-full bg-slate-300 p-3 rounded-md transition-shadow transition-colors hover:shadow-xl hover:bg-green-400">
+            <button
+              className="font-semibold text-xl text-slate-800"
+              onClick={() => onAdd(el)}
+            >
+              buy
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,14 +1,15 @@
-import Product from "./Product"
+import { useEffect } from "react";
+import Product from "./Product";
 
-const ProductList = ({onAdd,products}) => {
+const ProductList = ({ onAdd, products }) => {
   return (
-    <div >
-        <ul className="grid grid-cols-4 gap-5 p-6 ">
-            {products.map((el)=>(
-                <Product el={el} key={el.id} onAdd={onAdd}/>
-            ))}
-        </ul>
+    <div>
+      <ul className="grid grid-cols-4 gap-5 p-6 ">
+        {products.map((el) => (
+          <Product el={el} key={el.id} onAdd={onAdd} />
+        ))}
+      </ul>
     </div>
-  )
-}
-export default ProductList
+  );
+};
+export default ProductList;
