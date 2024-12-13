7eb4843c830d5d16d7ee6af8fcb827b70c4bb0f8
rezamovahedi <rmwhdy8@gmail.com>
2024-12-13 13:11:10 +0330

    add data and product list

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,7 +4,7 @@ import Product from "./Product";
 const ProductList = ({ onAdd, products }) => {
   return (
     <div>
-      <ul className="grid grid-cols-4 gap-5 p-6 ">
+      <ul className="grid grid-cols-4 gap-5 p-6">
         {products.map((el) => (
           <Product el={el} key={el.id} onAdd={onAdd} />
         ))}
--- src/components/data.js
+++
@@ -1,3 +0,0 @@
-import Axios from "axios"
-Axios.get("https://fakestoreapi.com/products")
-export default PRODUCT
