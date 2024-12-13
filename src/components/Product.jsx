24195e448199f03b42d8c08d96a21540eb0b92a7
rezamovahedi <rmwhdy8@gmail.com>
2024-12-13 13:10:51 +0330

    sliceing the title

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+// import '@rigo-m/react-split-text'
+import { SplitText } from "@rigo-m/react-split-text";
 const Product = ({ el, onAdd }) => {
+  const text = el.title;
   return (
     <div>
-      <div className="shadow-lg h-full  bg-gray-50 hover:bg-base-200 shadow-gray-400 rounded-2xl ">
-        <div className="">
+      <div className="shadow-lg h-auto bg-gray-50 hover:bg-base-200 shadow-gray-400 rounded-2xl ">
+        <div>
           <img
             className="w-16 p-1 m-2 md:w-32 lg:w-48"
             src={el.image}
-            width={100}
+            width={150}
             alt={el.image}
           />
-          <span>{el.title}</span>
+          <span>{text.slice(0, 19)}</span>
           <div className="flex justify-between">
             <span className="text-xs font-bold ml-2">{el.name}</span>
             <span className="text-red-600 font-semibold">{el.price}$</span>
           </div>
-          <div className="flex justify-center items-center w-full bg-slate-300 p-3 rounded-md transition-shadow transition-colors hover:shadow-xl hover:bg-green-400">
+          <div className="flex justify-center items-center w-full bg-slate-300 p-3 rounded-md  hover:shadow-xl hover:bg-green-400">
             <button
-              className="font-semibold text-xl  text-slate-800"
+              className="font-semibold text-xl w-auto h-auto  text-slate-800"
               onClick={() => onAdd(el)}
             >
               buy
