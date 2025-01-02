2e426c64b07859b59f7f6d8506a533441518447f
rezamovahedi <rmwhdy8@gmail.com>
2025-01-02 17:59:37 +0330

    changing sort item

--- src/components/listProduct/ProductList.jsx
+++ src/components/listProduct/ProductList.jsx
@@ -1,11 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import Product from "../products/Product";
 
 const ProductList = ({ onAdd, products }) => {
+  const [selectBy,setSelectBy] = useState('name')  
+  let selectedItems;
+  if(selectBy==='name') selectedItems = [...products].sort()
+  selectedItems = [...products].sort((a, b) => a.price - b.price)
   return (
     <div>
+      <div className="flex justify-center items-center mt-4">
+          <select 
+            value={selectBy}
+            onChange={e=> setSelectBy(e.target.value)}
+            className="border border-gray-300 rounded-md p-2"
+          >
+            <option value="price">Price</option>
+            <option value="name">Name</option>
+          </select>
+      </div>
       <ul className="grid grid-cols-4 gap-5 p-6">
-        {products.map((el) => (
+        {selectedItems.map((el) => (
           <Product el={el} key={el.id} onAdd={onAdd} />
         ))}
       </ul>
